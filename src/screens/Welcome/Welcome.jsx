525d27e4eeb595f804f8d48f885dbd23f4b62f13
retz8 <ekkicb71@gmail.com>
2023-04-19 12:32:28 -0400

    test

--- src/screens/Welcome/Welcome.jsx
+++ src/screens/Welcome/Welcome.jsx
@@ -12,6 +12,7 @@ import axios from "axios";
 import { Text } from "react-native";
 import { ANDROID_CLIENT_ID, IOS_CLIENT_ID, EXPO_CLIENT_ID } from "@env";
 import { getEvents, getFilteredEvents } from "../../api/event";
+import { getUser } from "../../api/user";
 
 WebBrowser.maybeCompleteAuthSession();
 LogBox.ignoreAllLogs();
@@ -101,6 +102,28 @@ function Welcome({ navigation }) {
     }
   }, [response]);
 
+  const handleLoginPress = async () => {
+    const { error, user } = await getUser("642ba16c1ed485ae197e6364");
+    if (error) console.log(error);
+    console.log(user);
+    setLoggedInUser(user);
+
+    // pageNum: 0, limit: 5,
+    const { error: eError, events } = await getEvents(0, 5);
+    if (eError) return console.log(error);
+    setInitialEvents(events);
+
+    // pageNum: 0, limit: 5 - Leaderboard,
+    const { error: f_error, events: f_events } = await getFilteredEvents(0, 5);
+    if (f_error) return console.log(error);
+    setInitialFilteredEvents(f_events);
+
+    console.log("successfully fetched user!");
+    console.log("Navigating to main page...");
+    setLogginIn(false);
+    navigation.push("MainStack");
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       {/* Login */}
@@ -120,10 +143,15 @@ function Welcome({ navigation }) {
         />
       ) : (
         // Login Image button
+        // <ImageButton
+        //   source={require("../../../assets/Welcome/loginButton.png")}
+        //   style={styles.imageButton}
+        //   onPress={() => promptAsync({ showInRecents: false })}
+        // />
         <ImageButton
           source={require("../../../assets/Welcome/loginButton.png")}
           style={styles.imageButton}
-          onPress={() => promptAsync({ showInRecents: false })}
+          onPress={handleLoginPress}
         />
       )}
     </SafeAreaView>
