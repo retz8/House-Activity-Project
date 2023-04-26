1512a0727cac1071ae9ef6ed4a5537df0b6cdee6
TedIwaArdN <edwardtian619@gmail.com>
2023-04-26 13:01:33 -0400

    Align items

--- src/components/LeaderBoardItem/LeaderBoardItem.jsx
+++ src/components/LeaderBoardItem/LeaderBoardItem.jsx
@@ -11,14 +11,71 @@
 //     enMotto: house english Motto,
 //     color: house color,
 // }
-import { Text, View } from "react-native";
-import React, { Component } from "react";
+import { Text, styleSheet, View } from "react-native";
+import React, { Component, useState } from "react";
 import styles from "./styles";
+import { getHouseByName } from "../../api/house";
 
 export default function LeaderBoardItem() {
-  return (
-    <View style={styles.container}>
-      <Text>Start Editing</Text>
+  const [ houses, setHouses ] = useState([]);
+  // const fetchHouses = async () => {
+  //   let houseArr = [];
+  //   const { house, error: aError } = await getHouseByName("Albemarle");
+  //   if (aError) return;
+  //   houseArr.push(house);
+  //   const { house, error: lError } = await getHouseByName("Lambert");
+  //   if (aError) return;
+  //   houseArr.push(house);
+  //   const { house, error: hError } = await getHouseByName("Hobler");
+  //   if (aError) return;
+  //   houseArr.push(house);
+  //   const { house, error: eError } = await getHouseByName("Ettl");
+  //   if (aError) return;
+  //   houseArr.push(house);
+
+
+  // }
+
+    // useEffect(() => {
+    //   fetchHouses();
+    // }, [])
+
+    /*
+     * 
+     * **************************************************************
+     * ***************Provide House points over here*****************
+     * **************************************************************
+     * 
+     */
+
+    let housePoints_a = 1403, housePoints_l = 1451, housePoints_h = 1705, housePoints_e = 1285;
+
+    /* 
+     * 
+     * **************************************************************
+     * ***************Provide House points over here*****************
+     * **************************************************************
+     * 
+     */
+
+    return (
+        <View style={[styles.container, { flexDirection: 'row', }, ]}>
+        <View style={styles.container}>
+                <Text>Albemarle</Text>
+                <Text>{housePoints_a}</Text>
+        </View>
+        <View style={styles.container}>
+                <Text>Lambert</Text>
+                <Text>{housePoints_l}</Text>
+        </View>
+        <View style={styles.container}>
+                <Text>Hobler</Text>
+                <Text>{housePoints_h}</Text>
+        </View>
+        <View style={styles.container}>
+                <Text>Ettl</Text>
+                <Text>{housePoints_e}</Text>
+        </View>
     </View>
   );
 }
