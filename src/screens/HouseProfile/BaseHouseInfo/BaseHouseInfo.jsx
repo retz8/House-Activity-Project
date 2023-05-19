de46e5c262d31feba24e200665414a73bd284373
retz8 <ekkicb71@gmail.com>
2023-05-18 20:26:05 -0400

    comment deleted

--- src/screens/HouseProfile/BaseHouseInfo/BaseHouseInfo.jsx
+++ src/screens/HouseProfile/BaseHouseInfo/BaseHouseInfo.jsx
@@ -4,12 +4,6 @@ import styles from "./styles";
 
 export default function BaseHouseInfo({ crest, name, point, color }) {
   return (
-    // <View style={styles.container}>
-    // <Image style={styles.crest} source={{ uri: crest.url }} alt="" />
-    // <Text style={styles.houseName}>{name}</Text>
-
-    // <Text style={styles.points}>{point}</Text>
-    // </View>
     <View style={styles.container}>
       <View style={styles.image}>
         <Image style={styles.crest} source={{ uri: crest.url }} alt="" />
